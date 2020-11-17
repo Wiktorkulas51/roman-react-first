9aa10422a991b64f078c5461bb8faef2aa79930d
Wiktorkulas51 <wiktorkulas51@gmail.com>
2020-11-17 18:14:16 +0100

    initial commit

--- 04/src/App.js
+++ 04/src/App.js
@@ -5,11 +5,12 @@ import Form from "./components/Form/Form";
 
 const initialStateItems = [
   {
-    image: "https://pbs.twimg.com/profile_images/906557353549598720/oapgW_Fp.jpg",
+    image:
+      "https://d2eip9sf3oo6c2.cloudfront.net/instructors/avatars/000/000/032/medium/oapgW_Fp_400x400.jpg",
     name: "Dan Abramov",
     description: "React core member",
-    twitterLink: "https://twitter.com/dan_abramov"
-  }
+    twitterLink: "https://twitter.com/dan_abramov",
+  },
 ];
 
 class App extends React.Component {
@@ -17,18 +18,18 @@ class App extends React.Component {
     items: [...initialStateItems],
   };
 
-  addItem = e => {
+  addItem = (e) => {
     e.preventDefault();
 
     const newItem = {
       name: e.target[0].value,
       twitterLink: e.target[1].value,
       image: e.target[2].value,
-      description: e.target[3].value
+      description: e.target[3].value,
     };
 
-    this.setState(prevState => ({
-      items: [...prevState.items, newItem]
+    this.setState((prevState) => ({
+      items: [...prevState.items, newItem],
     }));
 
     e.target.reset();
